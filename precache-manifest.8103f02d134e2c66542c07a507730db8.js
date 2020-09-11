b68d48ef3fb951cc4954548248c91d0007c90b23
Fazzani <heni.fazzani@gmail.com>
2020-09-12 01:12:25 +0200

    Updates

---
+++ precache-manifest.8103f02d134e2c66542c07a507730db8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d17d300abef2ef0a47d6e370932db67",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "c009d829c80e29fa49d8",
+    "url": "/todoapp/static/css/main.3d0435de.chunk.css"
+  },
+  {
+    "revision": "bdae4d4274d1bcb6c9eb",
+    "url": "/todoapp/static/js/2.d50da62a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todoapp/static/js/2.d50da62a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c009d829c80e29fa49d8",
+    "url": "/todoapp/static/js/main.fa488450.chunk.js"
+  },
+  {
+    "revision": "80b05f8f46ecf0bf2c2b",
+    "url": "/todoapp/static/js/runtime-main.5545d9fc.js"
+  }
+]);
