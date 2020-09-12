6fab6416bc4b014b8dea2c9664b7c3af964196b6
Fazzani <heni.fazzani@gmail.com>
2020-09-12 12:21:10 +0200

    add detail

--- src/components/card.grid.js
+++ src/components/card.grid.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { Card } from './card';
 import { CardGridHeader } from './card-grid-header';
 import { Description } from './description';
