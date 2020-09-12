793f55a7831e5c371eae3d2be7c3b91beb39bdbf
Fazzani <heni.fazzani@gmail.com>
2020-09-12 12:17:36 +0200

    Updates

---
+++ precache-manifest.32c632ec7c0b2975a726a6404fbdf7d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69232825d512bf399c6980e2ae88a0ca",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "0c4e7656dfd41ad55c5a",
+    "url": "/cv/static/css/main.6cc50e97.chunk.css"
+  },
+  {
+    "revision": "9b24fcf34c6df14a52cf",
+    "url": "/cv/static/js/2.2d0dc39b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/cv/static/js/2.2d0dc39b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0c4e7656dfd41ad55c5a",
+    "url": "/cv/static/js/main.fe6ab74c.chunk.js"
+  },
+  {
+    "revision": "be496cfe662947d5bae6",
+    "url": "/cv/static/js/runtime-main.f37fbf2a.js"
+  },
+  {
+    "revision": "9c7c89cc54cd2f2ad793765259d23cad",
+    "url": "/cv/static/media/cv.9c7c89cc.pdf"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cv/precache-manifest.5436572ae37f882f11e030bd18a950fc.js"
+  "/cv/precache-manifest.32c632ec7c0b2975a726a6404fbdf7d4.js"
 );
 
 self.addEventListener('message', (event) => {
