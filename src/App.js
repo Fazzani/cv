8c55937229faea81b1c28b76c509b5fbd1b2c8d1
Fazzani <heni.fazzani@gmail.com>
2020-09-12 12:16:58 +0200

    add detail

--- src/App.js
+++ src/App.js
@@ -2,6 +2,12 @@ import React from 'react';
 import './App.css';
 import CardGrid  from './components/card.grid';
 
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { fab } from '@fortawesome/free-brands-svg-icons'
+import { fas } from '@fortawesome/free-solid-svg-icons'
+
+library.add(fab, fas)
+
 function App() {
   return (
     <div className="App">
--- src/components/card-grid-header.js
+++ src/components/card-grid-header.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import './card-grid-header-styles.css';
 import picture from '../assets/me.jpg';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import cvpdf from '../assets/cv.pdf';
 
 export const CardGridHeader = ({ candidate }) => {
   if (candidate)
@@ -9,12 +11,25 @@ export const CardGridHeader = ({ candidate }) => {
         <h2 className="header-card-title">
           {candidate.FirstName} {candidate.LastName}
         </h2>
-        <img className="picture blur" src={picture} alt="Heni"/>
+        <img className="picture" src={picture} alt="Heni" />
         <p className="header-card-sponsor">
-          {candidate.Email} <br />
-          {candidate.Tel} <br />
-          {candidate.Address.Postal} {candidate.Address.City}
+          <FontAwesomeIcon icon={['fas', 'at']} size="1x" /> {candidate.Email} <br />
+          <FontAwesomeIcon icon={['fas', 'phone-square']} size="1x" /> {candidate.Tel} <br />
+          <FontAwesomeIcon icon={['fas', 'map-marker-alt']} size="1x" /> {candidate.Address.Postal} {candidate.Address.City}
         </p>
+        <div className="social" title="Github account">
+          <a href={candidate.Github}>
+            <FontAwesomeIcon icon={['fab', 'github']} size="1x" color="black" />
+          </a>
+
+          <a href={candidate.Linkedin} title="Linkedin account">
+            <FontAwesomeIcon icon={['fab', 'linkedin']} size="1x" color="black" />
+          </a>
+
+          <a href={cvpdf} alt="CV" title="Download CV">
+            <FontAwesomeIcon icon={['fas', 'file-download']} size="2x" color="black" />
+          </a>
+        </div>
       </header>
     );
   else return null;
